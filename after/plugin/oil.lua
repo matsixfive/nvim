fc689e6453542beeb3e6b87b36dc61eb29757907
matsixfive <maffers2006@gmail.com>
2023-06-29 21:57:09 +0100

    oil bindings and no copilot for oil

--- after/plugin/oil.lua
+++ after/plugin/oil.lua
@@ -6,17 +6,15 @@ require('oil').setup({
 	keymaps = {
     ["g?"] = "actions.show_help",
     ["<CR>"] = "actions.select",
-    -- ["<C-s>"] = "actions.select_vsplit",
+    ["<C-s>"] = "actions.select_vsplit",
     -- ["<C-h>"] = "actions.select_split",
     -- ["<C-t>"] = "actions.select_tab",
     -- ["<C-p>"] = "actions.preview",
-    ["<C-c>"] = "actions.close",
+    ["<Esc>"] = "actions.close",
     ["<C-l>"] = "actions.refresh",
     ["-"] = "actions.parent",
     ["_"] = "actions.open_cwd",
-    -- ["`"] = "actions.cd",
-    -- ["~"] = "actions.tcd",
-    -- ["g."] = "actions.toggle_hidden",
+    ["g."] = "actions.toggle_hidden",
   },
 
 	view_options = {
