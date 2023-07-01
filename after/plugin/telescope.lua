e98e8068a1a45417493a1cefcf21c13bd6d47495
matsixfive <maffers2006@gmail.com>
2023-07-01 12:46:21 +0100

    telescope fallback when no git

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,6 +1,15 @@
 local builtin = require('telescope.builtin')
+local utils = require('telescope.utils')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+vim.keymap.set('n', '<C-p>',
+	function()
+    local _, ret, _ = utils.get_os_command_output({ 'git', 'rev-parse', '--is-inside-work-tree' })
+    if ret == 0 then
+        builtin.git_files()
+    else
+        builtin.find_files()
+    end
+	end,{})
 -- vim.keymap.set('n', '<leader>ps', function()
 -- 	builtin.grep_string({ search = vim.fn.input("Grep > ") })
 -- end)
