20660b8ff84ece01d2f1faa4f360655084948d9c
matsixfive <maffers2006@gmail.com>
2023-07-25 15:09:40 +0100

    trying to get windows to work

--- lua/matsixfive/packer.lua
+++ lua/matsixfive/packer.lua
@@ -60,20 +60,17 @@ return require('packer').startup(
 
 		use 'lewis6991/gitsigns.nvim'
 
-		use({
-			"glepnir/lspsaga.nvim",
-			opt = true,
-			branch = "main",
-			event = "LspAttach",
-			config = function()
-				require("lspsaga").setup({})
-			end,
-			requires = {
-				{"nvim-tree/nvim-web-devicons"},
-				--Please make sure you install markdown and markdown_inline parser
-				{"nvim-treesitter/nvim-treesitter"}
-			}
-		})
+		-- use({
+		-- 	"glepnir/lspsaga.nvim",
+		-- 	opt = true,
+		-- 	branch = "main",
+		-- 	event = "LspAttach",
+		-- 	requires = {
+		-- 		{"nvim-tree/nvim-web-devicons"},
+		-- 		--Please make sure you install markdown and markdown_inline parser
+		-- 		{"nvim-treesitter/nvim-treesitter"}
+		-- 	}
+		-- })
 
 		use 'stevearc/oil.nvim'
 
