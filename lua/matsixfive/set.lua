055a40aadfa1fe32281be36d6bf9be85eff8070f
matsixfive <maffers2006@gmail.com>
2023-09-07 14:24:38 +0100

    Revert "trying to get windows to work"
    This reverts commit 20660b8ff84ece01d2f1faa4f360655084948d9c.

--- lua/matsixfive/set.lua
+++ lua/matsixfive/set.lua
@@ -8,7 +8,7 @@ vim.optexpandtab = false
 
 vim.opt.swapfile = false
 vim.opt.backup = false
--- vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
 vim.opt.hlsearch = false
