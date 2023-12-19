f60416aed03d6e5dbce74350783c9729f3218704
matsixfive <maffers2006@gmail.com>
2023-12-19 17:03:50 +0000

    icon changes

--- after/plugin/devicons.lua
+++ after/plugin/devicons.lua
@@ -1,36 +1,8 @@
--- require('nvim-web-devicons').setup {
--- 	color_icons = true,
--- 	-- default = true,
--- 	-- strict = true,
--- 	-- override = {
--- 	-- 	txt = {
--- 	-- 		icon = ''
--- 	-- 	},
--- 	-- 	html = {
--- 	-- 		icon = "",
--- 	-- 		color="#dd4b25",
--- 	-- 		name="Html"
--- 	-- 	}
--- 	-- },
--- 	override = {
--- 		html = { icon = "", color = "#DE8C92", name = "html" },
--- 		-- Other file type overrides...
--- 	},
--- 	override_by_filename = {
--- 		["Pipfile"] = {
--- 			icon = ""
--- 		}
--- 	},
--- 	override_by_extension = {
--- 		["oil"] = {
--- 			icon = ""
--- 		},
--- 	}
--- }
 require('nvim-web-devicons').setup {
   override = {
     html = { icon = "", color = "#dd4b25", name = "html" },
 		txt = { icon = "", color = "#DE8C92", name = "txt" },
+		toml = { icon = "", color = "#e6b943", name = "toml" },
   },
 	override_by_extension = {
 		["oil"] = {
@@ -38,7 +10,6 @@ require('nvim-web-devicons').setup {
 			color = "#ffffff",
 			name = "Oil"
 		},
-		["svelte"] = { icon = "", color="#ff0000", name = "Svelte"}
 	},
 	override_by_filename = {
 		["oil"] = {
@@ -51,4 +22,5 @@ require('nvim-web-devicons').setup {
 
 require('nvim-web-devicons').set_default_icon('', '#a9b1d6', 153)
 
--- S         󰒯   
+--    
+--   
