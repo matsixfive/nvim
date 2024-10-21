33299206ed4cd72a98501a28e9cb43d7fd43d3c0
matsixfive <maffers2006@gmail.com>
2024-10-21 09:43:22 +0100

    disable copilot for files with  in first line

--- lua/matsixfive/autocmd.lua
+++ lua/matsixfive/autocmd.lua
@@ -1,9 +1,37 @@
 local autocmd = vim.api.nvim_create_autocmd
 
 autocmd("TextYankPost", {
-  callback = function()
-    require("vim.highlight").on_yank { higroup = "IncSearch", timeout = 100 }
-  end,
-  group = general,
-  desc = "Highlight when yanking",
+	callback = function()
+		require("vim.highlight").on_yank { higroup = "IncSearch", timeout = 100 }
+	end,
+	group = general,
+	desc = "Highlight when yanking",
 })
+
+autocmd("BufEnter", {
+	callback = function(args)
+		local bufnr = args.buf
+
+		local fisrt_line = vim.fn.getbufoneline(bufnr, 1)
+		local is_university_file = string.match(fisrt_line, "!!uni")
+		if not is_university_file then
+			return
+		end
+		-- Disable copilot
+		vim.cmd("Copilot disable")
+
+		local filetype = vim.bo.filetype
+		UniOptTable[filetype]()
+	end,
+	group = general,
+	desc = "Disable copilot for university files",
+})
+
+UniOptTable = {
+	["java"] = function()
+		vim.opt.tabstop = 4
+		vim.opt.shiftwidth = 4
+		vim.opt.expandtab = true
+		vim.opt.colorcolumn = "80"
+	end,
+}
