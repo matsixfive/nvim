8177b8b59969376f0433f2358e0e7e01c1d11556
matsixfive <maffers2006@gmail.com>
2025-10-21 15:46:13 +0100

    blame settings

--- lua/plugins/blame.lua
+++ lua/plugins/blame.lua
@@ -6,5 +6,9 @@ end, { noremap = true, silent = true })
 return {
 	"FabijanZulj/blame.nvim",
 	lazy = true,
-	opts = {},
+	opts = {
+		date_format = "%d-%m-%Y",
+		focus_blame = false,
+		merge_consecutive = false,
+	},
 }
