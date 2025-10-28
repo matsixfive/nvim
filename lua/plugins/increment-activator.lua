e619b1a12bc7231c404d6597969501e4fde8064c
matsixfive <maffers2006@gmail.com>
2025-10-28 10:24:49 +0000

    ia

--- lua/plugins/increment-activator.lua
+++ lua/plugins/increment-activator.lua
@@ -1,31 +1,30 @@
+vim.g.increment_activator_filetype_candidates = {
+	["_"] = {
+		{ "up",    "down" },
+		{ "left",  "right" },
+		{ "first", "last" },
+	},
+	["css"] = {
+		{ "inside",        "outside" },
+		{ "ltr",           "rtl" },
+		{ "min-content",   "max-content" },
+		{ "nowrap",        "wrap",         "wrap-reverse" },
+		{ "show",          "hide" },
+		{ "static",        "relative",     "absolute",    "fixed", "sticky" },
+		{ "underline",     "overline",     "line-through" },
+		{ "row",           "column" },
+		{ "dark",          "light" },
+		{ "inline",        "inline-block" },
+		{ "block",         "inline-block" },
+		{ "space-between", "space-around", "space-evenly" },
+		{ "visible",       "hidden" },
+	}
+}
+
 return {
 	'nishigori/increment-activator',
 	keys = {
 		"<C-a>",
 		"<C-x>",
 	},
-	config = function()
-		vim.g.increment_activator_filetype_candidates = {
-			["_"] = {
-				{ "up",    "down" },
-				{ "left",  "right" },
-				{ "first", "last" },
-			},
-			["css"] = {
-				{ "inside",        "outside" },
-				{ "ltr",           "rtl" },
-				{ "min-content",   "max-content" },
-				{ "nowrap",        "wrap",         "wrap-reverse" },
-				{ "show",          "hide" },
-				{ "static",        "relative",     "absolute",    "fixed", "sticky" },
-				{ "underline",     "overline",     "line-through" },
-				{ "row",           "column" },
-				{ "dark",          "light" },
-				{ "inline",        "inline-block" },
-				{ "block",         "inline-block" },
-				{ "space-between", "space-around", "space-evenly" },
-				{ "visible",       "hidden" },
-			}
-		}
-	end,
 }
