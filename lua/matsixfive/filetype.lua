041c1ca0af55985f5845c70f4262b8b5e9218a41
matsixfive <maffers2006@gmail.com>
2025-10-29 13:04:37 +0000

    ahk

--- lua/matsixfive/filetype.lua
+++ lua/matsixfive/filetype.lua
@@ -2,13 +2,14 @@ vim.filetype.add({
 	extension = {
 		mdx = "mdx",
 		templ = "templ",
+		ah2 = "autohotkey",
 	},
 
 	filename = {
 		["TODO"] = "markdown",
 		[".git/config"] = "gitconfig",
 		["README$"] = function(path, bufnr)
-			if string.find("#", vim.api.nvim_buf_get_lines(bufnr, 0, 1, true)) then
+			if string.find("#", vim.api.nvim_buf_get_lines(bufnr, 0, 1, true)[1]) then
 				return "markdown"
 			end
 			-- no return means the filetype won't be set and to try the next method
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -8,44 +8,46 @@
 -- )
 
 return {
-	'nvim-treesitter/nvim-treesitter',
-	build = ':TSUpdate',
-	main = 'nvim-treesitter.config',
-	lazy = false,
-	config = function()
-		require('nvim-treesitter.configs').setup {
-			ensure_installed = {
-				"lua",
-				"rust",
-				"html",
-				"css",
-				"comment",
-				"jsdoc",
-			},
-			sync_install = false,
-			auto_install = true,
+	{
+		'nvim-treesitter/nvim-treesitter',
+		build = ':TSUpdate',
+		main = 'nvim-treesitter.config',
+		lazy = false,
+		config = function()
+			require('nvim-treesitter.configs').setup {
+				ensure_installed = {
+					"lua",
+					"rust",
+					"html",
+					"css",
+					"comment",
+					"jsdoc",
+				},
+				sync_install = false,
+				auto_install = true,
 
-			highlight = {
-				enable = true,
-				additional_vim_regex_highlighting = false,
-			},
-		}
+				highlight = {
+					enable = true,
+					additional_vim_regex_highlighting = false,
+				},
+			}
 
-		vim.treesitter.language.register("markdown", "mdx")
+			vim.treesitter.language.register("markdown", "mdx")
 
-		local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-		parser_config.mcfunction = {
-			install_info = {
-				url = "https://github.com/theusaf/tree-sitter-mcfunction-lang", -- local path or git repo
-				files = { "src/parser.c" },                         -- note that some parsers also require src/scanner.c or src/scanner.cc
-			},
-		}
+			local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+			parser_config.mcfunction = {
+				install_info = {
+					url = "https://github.com/theusaf/tree-sitter-mcfunction-lang", -- local path or git repo
+					files = { "src/parser.c" },                                -- note that some parsers also require src/scanner.c or src/scanner.cc
+				},
+			}
 
-		-- .mcfunction filetype
-		vim.filetype.add({
-			extension = {
-				mcfunction = "mcfunction",
-			},
-		})
-	end
+			-- .mcfunction filetype
+			vim.filetype.add({
+				extension = {
+					mcfunction = "mcfunction",
+				},
+			})
+		end,
+	}
 }
