bc846e261f0f58cebb05ffbd26201c5b98d97d4d
matsixfive <maffers2006@gmail.com>
2025-11-18 23:58:12 +0000

    ft stuff

--- after/ftplugin/java.lua
+++ after/ftplugin/java.lua
@@ -3,5 +3,5 @@ if vim.g.is_uni == true then
 	vim.opt_local.shiftwidth = 4
 	vim.opt_local.expandtab = true
 	vim.opt_local.colorcolumn = "80"
-	vim.b.length_check = true
+	vim.b.length_check = false
 end
