e96d244534cda07fbb19680351ee053af349d7c0
matsixfive <maffers2006@gmail.com>
2026-04-02 18:46:23 +0100

    use lazy kemaps for telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,66 +1,63 @@
-vim.keymap.set("n", "<leader>vh",
-	require("telescope.builtin").help_tags,
-	{ desc = "Telescope vim help" }
-)
-
-vim.keymap.set("n", "<leader>pg",
-	require("telescope.builtin").live_grep,
-	{ desc = "Telescope live grep" }
-)
-vim.keymap.set("n", "<leader>pf",
-	require("telescope.builtin").find_files,
-	{ desc = "Telescope find files" }
-)
-
-vim.keymap.set("n", "<leader>pd",
-	require("telescope.builtin").diagnostics,
-	{ desc = "Telescope diagnostic" }
-)
-vim.keymap.set("n", "<leader>pe",
-	function()
-		require("telescope.builtin").diagnostics({ severity = "error" })
-	end,
-	{ desc = "Telescope errors" }
-)
-
-vim.keymap.set("n", "<leader>cs",
-	function()
-		require("telescope.builtin").colorscheme({ enable_preview = true })
-	end,
-	{ desc = "Telescope colorscheme" }
-)
-
-vim.keymap.set("n", "<C-p>",
-	function()
-		local _, ret, _ = require("telescope.utils").get_os_command_output({ "git", "rev-parse", "--is-inside-work-tree" })
-		if ret == 0 then
-			require("telescope.builtin").git_files()
-		else
-			require("telescope.builtin").find_files()
-		end
-	end, { desc = "Telescope git files" }
-)
-
 return {
-	{
-		"nvim-telescope/telescope.nvim",
-		lazy = true,
-		opts = {
-			defaults = {
-				dynamic_preview_title = true,
-				path_display = { "truncate", "filename_first" },
-				mappings = {
-					i = {
-						["<C-j>"] = require("telescope.actions").move_selection_next,
-						["<C-k>"] = require("telescope.actions").move_selection_previous,
-					}
-				},
-			},
-		}
-	},
-	{
-		"nvim-telescope/telescope-ui-select.nvim",
-		dependencies = { "nvim-telescope/telescope.nvim", },
-		lazy = true
-	}
+    {
+        "nvim-telescope/telescope.nvim",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        -- This tells lazy.nvim: "Only load this plugin when I press one of these keys"
+        keys = {
+            { "<leader>vh", "<cmd>Telescope help_tags<cr>", desc = "Telescope vim help" },
+            { "<leader>pg", "<cmd>Telescope live_grep<cr>", desc = "Telescope live grep" },
+            { "<leader>pf", "<cmd>Telescope find_files<cr>", desc = "Telescope find files" },
+            { "<leader>pd", "<cmd>Telescope diagnostics<cr>", desc = "Telescope diagnostic" },
+            { 
+                "<leader>pe", 
+                function() require("telescope.builtin").diagnostics({ severity = "error" }) end, 
+                desc = "Telescope errors" 
+            },
+            { 
+                "<leader>cs", 
+                function() require("telescope.builtin").colorscheme({ enable_preview = true }) end, 
+                desc = "Telescope colorscheme" 
+            },
+            {
+                "<C-p>",
+                function()
+                    local _, ret, _ = require("telescope.utils").get_os_command_output({ "git", "rev-parse", "--is-inside-work-tree" })
+                    if ret == 0 then
+                        require("telescope.builtin").git_files()
+                    else
+                        require("telescope.builtin").find_files()
+                    end
+                end,
+                desc = "Telescope git files",
+            },
+        },
+        config = function(_, opts)
+            local telescope = require("telescope")
+            local actions = require("telescope.actions")
+
+            -- We define mappings here because they require the 'telescope.actions' module
+            opts.defaults.mappings = {
+                i = {
+                    ["<C-j>"] = actions.move_selection_next,
+                    ["<C-k>"] = actions.move_selection_previous,
+                },
+            }
+
+            telescope.setup(opts)
+        end,
+        opts = {
+            defaults = {
+                dynamic_preview_title = true,
+                path_display = { "truncate", "filename_first" },
+            },
+        },
+    },
+    {
+        "nvim-telescope/telescope-ui-select.nvim",
+        -- Ensure ui-select only loads when telescope itself loads
+        event = "DeferredConfig", 
+        config = function()
+            require("telescope").load_extension("ui-select")
+        end,
+    },
 }
