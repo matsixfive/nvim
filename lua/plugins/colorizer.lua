4d7716dc98557b6e044edc4e2ee0d05c251ac35e
matsixfive <maffers2006@gmail.com>
2026-05-05 16:07:45 +0100

    lazy colorizer

--- lua/plugins/colorizer.lua
+++ lua/plugins/colorizer.lua
@@ -1,8 +1,12 @@
 return {
 	"norcalli/nvim-colorizer.lua",
+	ft = {
+		"html",
+		"css",
+		"scss",
+		"typst"
+	},
 	opts = {
-		["*"] = { mode = "background" },
-
 		css = { rgb_fn = true, }, -- Enable `rgb()` functions in CSS
 		html = { names = true, }, -- Enable color names like "red"
 	}
